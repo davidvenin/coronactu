86814fc1df093689bdb40fde15be60389a16c190
David <david@hostnfly.com>
2021-12-19 16:14:22 +0100

    feat: updates views to compose with components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import "../glossy/globals.css";
+
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from "next";
+import { SearchForm } from "../components/search-form";
 
 const Home: NextPage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className={"container"}>
+      <SearchForm />
+    </div>
+  );
 };
 
 export default Home;
