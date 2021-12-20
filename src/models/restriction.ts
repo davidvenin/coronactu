72f11450648f33cd25d537028a8df2c64e31e972
David <david@hostnfly.com>
2021-12-20 17:03:29 +0100

    add new models for restrictions

---
+++ src/models/restriction.ts
@@ -0,0 +1,38 @@
+export interface RestrictionsPayload {
+  included: Included[];
+}
+
+export interface Included {
+  id: string;
+  type: string;
+  attributes: Attributes;
+}
+
+export interface DocumentLink {
+  type: string;
+  url: string;
+  title: string;
+}
+
+export interface Attributes {
+  country: string;
+  title: string;
+  description: string;
+  more: string[];
+  severity: number;
+  documentLinks: DocumentLink[];
+  tags: string[];
+  source: Source;
+  lastUpdatedAt: Date;
+  createdAt: Date;
+  startDate: Date;
+  endDate: null;
+  category: string;
+  subCategory: string;
+}
+
+export interface Source {
+  sourceType: string;
+  title: string;
+  url: string;
+}
