aa31bc93790d44509c459b3ccbe3e93d75a4d660
David <david@hostnfly.com>
2021-12-20 16:55:31 +0100

    feat: add new api routes for restrictions purpose

--- src/constants/api-routes.ts
+++ src/constants/api-routes.ts
@@ -1,4 +1,6 @@
 export const api = {
   countries_api: (q?: string) =>
     `https://api.first.org/data/v1/countries?q=${q}`,
+  restrictions: (from: string, to: string) =>
+    `https://ulysse-tooling-api.herokuapp.com/api/sherpa/country?origin=${from}&destination=${to}`,
 };
