2b9f9cb97458f7f123b02f4ea09f3ea900154490
David <david@hostnfly.com>
2021-12-21 12:39:05 +0100

    feat: display date (wrong ones) and add know more toggle

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,4 +1,5 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
+import { format } from "date-fns";
 import { Included } from "../../models/restriction";
 
 interface Props {
@@ -6,18 +7,29 @@ interface Props {
 }
 
 export const Card: FunctionComponent<Props> = ({ restriction }: Props) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
-    <div className="shadow-high bg-gray-light rounded-5 py-10 rounded-tl-none pb-30 pt-30 pl-20 pr-20">
-      <div className={"flex items-center mb-20"}>
+    <div className="shadow-high bg-gray-light rounded-5 py-10 rounded-tl-none pb-30 pt-30 pl-20 pr-20 text-14">
+      <div className={"flex items-center mb-10"}>
         <strong className="text-12">{restriction.attributes.country}</strong>
         <h3 className="ml-16 font-semibold ml-5">
           {restriction.attributes.title}
         </h3>
       </div>
+      <div className={"mb-10"}>
+        <span className={"block text-12 text-gray-70"}>
+          Mise en vigueur : {format(new Date(restriction.attributes.createdAt), "dd-mm-yyyy")}
+        </span>
+        <span className={"block text-12 text-gray-70"}>
+          Dernière mise à jour : {format(new Date(restriction.attributes.lastUpdatedAt), "dd-mm-yyyy")}
+        </span>
+      </div>
       {restriction.attributes.description && (
         <p className="text-14">{restriction.attributes.description}</p>
       )}
-      {restriction.attributes.more && <p>{restriction.attributes.more}</p>}
+      {restriction.attributes.more[0] && !isOpen && <p className={"font-semibold cursor-pointer"} onClick={() => setIsOpen(true)}>En savoir plus</p>}
+      {restriction.attributes.more[0] && isOpen && <p>{restriction.attributes.more}</p>}
+      {restriction.attributes.more[0] && isOpen && <p className={"font-semibold cursor-pointer"} onClick={() => setIsOpen(false)}>En voir moins</p>}
       <div>
         <h4>Source</h4>
         <a
