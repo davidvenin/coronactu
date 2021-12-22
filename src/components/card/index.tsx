f0120a9227f1bda8be71c87f8e7238007e2d3a05
David <david@hostnfly.com>
2021-12-22 12:29:08 +0100

    feat: add modal

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState } from "react";
+import React, { FunctionComponent } from "react";
 import { format } from "date-fns";
 import { Included } from "../../models/restriction";
 import { useModal } from "../../hooks/use-modal";
@@ -8,66 +8,80 @@ interface Props {
 }
 
 export const Card: FunctionComponent<Props> = ({ restriction }: Props) => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const { show, hide, RenderModal } = useModal();
+  const { show, RenderModal } = useModal();
   return (
-    <div
-      onClick={() => {
-        setIsOpen(true);
-        show();
-      }}
-      className="shadow-high bg-gray-light rounded-5 py-10 rounded-tl-none pb-30 pt-30 pl-20 pr-20 text-14 max-h-300"
-    >
-      <div className={"flex items-center mb-10"}>
-        <strong className="text-12">{restriction.attributes.country}</strong>
-        <h3 className="ml-16 font-semibold ml-5">
-          {restriction.attributes.title}
-        </h3>
-      </div>
-      <div className={"mb-10"}>
-        <span className={"block text-12 text-gray-70"}>
-          Mise en vigueur :{" "}
-          {format(new Date(restriction.attributes.createdAt), "dd-mm-yyyy")}
-        </span>
-        <span className={"block text-12 text-gray-70"}>
-          Dernière mise à jour :{" "}
-          {format(new Date(restriction.attributes.lastUpdatedAt), "dd-mm-yyyy")}
-        </span>
-      </div>
-      {restriction.attributes.description && (
-        <p className="text-14 line-clamp-4">
-          {restriction.attributes.description}
-        </p>
-      )}
-      <div>
-        <h4>Source</h4>
-        <a
-          href={restriction.attributes.source.url}
-          target="_blank"
-          rel="noreferrer"
-          className="text-12 text-red hover:text-dark"
-        >
-          {restriction.attributes.source.title}
-        </a>
+    <>
+      <div
+        onClick={show}
+        className="shadow-high bg-gray-light rounded-5 py-10 rounded-tl-none pb-30 pt-30 pl-20 pr-20 text-14 max-h-300 cursor-pointer"
+      >
+        <div className={"flex items-center mb-10"}>
+          <strong className="text-12">{restriction.attributes.country}</strong>
+          <h3 className="ml-16 font-semibold ml-5">
+            {restriction.attributes.title}
+          </h3>
+        </div>
+        <div className={"mb-10"}>
+          <span className={"block text-12 text-gray-70"}>
+            Mise en vigueur :{" "}
+            {format(new Date(restriction.attributes.createdAt), "dd-mm-yyyy")}
+          </span>
+          <span className={"block text-12 text-gray-70"}>
+            Dernière mise à jour :{" "}
+            {format(
+              new Date(restriction.attributes.lastUpdatedAt),
+              "dd-mm-yyyy"
+            )}
+          </span>
+        </div>
+        {restriction.attributes.description && (
+          <p className="text-14 line-clamp-4">
+            {restriction.attributes.description}
+          </p>
+        )}
+        <div>
+          <a
+            href={restriction.attributes.source.url}
+            target="_blank"
+            rel="noreferrer"
+            className="text-12 text-red hover:text-dark"
+          >
+            {restriction.attributes.source.title}
+          </a>
+        </div>
       </div>
       <RenderModal>
         <>
-          {restriction.attributes.more[0] && isOpen && (
-            <p>{restriction.attributes.more}</p>
-          )}
-          {restriction.attributes.more[0] && isOpen && (
-            <p
-              className={"font-semibold cursor-pointer"}
-              onClick={() => {
-                setIsOpen(false);
-                hide();
-              }}
-            >
-              En voir moins
+          <div className={"flex items-center mb-10"}>
+            <strong className="text-12">
+              {restriction.attributes.country}
+            </strong>
+            <h3 className="ml-16 font-semibold ml-5">
+              {restriction.attributes.title}
+            </h3>
+          </div>
+          {restriction.attributes.description && (
+            <p className="text-14 line-clamp-4">
+              {restriction.attributes.description}
             </p>
           )}
+          {restriction.attributes.more[0] && (
+            <p className={"text-14 mt-20 mb-20"}>
+              {restriction.attributes.more}
+            </p>
+          )}
+          <div>
+            <a
+              href={restriction.attributes.source.url}
+              target="_blank"
+              rel="noreferrer"
+              className="text-12 text-red hover:text-dark"
+            >
+              {restriction.attributes.source.title}
+            </a>
+          </div>
         </>
       </RenderModal>
-    </div>
+    </>
   );
 };
--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -12,12 +12,32 @@ export const Modal = memo(({ children, closeModal }: Props) => {
   if (!domEl) return null;
 
   return createPortal(
-    <div className="fixed w-full bottom-0 top-0 left-0 right-0 bg-white-40">
-      <div className="bg-white m-10 p-20">
-        <div>
-          <span onClick={closeModal} />
+    <div className="fixed w-full bottom-0 top-0 left-0 right-0 bg-white-90">
+      <div className="relative">
+        <div className="bg-white p-20 md:w-400 mx-auto mt-50">
+          <div>
+            <span
+              onClick={closeModal}
+              className="block w-30 h-30 cursor-pointer ml-auto"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </span>
+          </div>
+          {children}
         </div>
-        {children}
       </div>
     </div>,
     domEl
