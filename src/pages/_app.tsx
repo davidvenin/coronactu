985ad5d7dc31ba3c68b0cca39d54d31c760ffc12
David <david@hostnfly.com>
2021-12-22 22:37:03 +0100

    feat: add title on page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import "../glossy/globals.css";
-
+import Head from "next/head";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <title>Coronactu - Tout ce que vous devez savoir avant de partir</title>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
