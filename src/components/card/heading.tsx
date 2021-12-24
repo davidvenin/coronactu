43d6d23799f639b6e8c38ff369c9cf7b49e63b83
David <david@hostnfly.com>
2021-12-24 14:04:58 +0100

    fix: better compositin

---
+++ src/components/card/heading.tsx
@@ -0,0 +1,24 @@
+import React, { FunctionComponent } from "react";
+import { format } from "date-fns";
+import { fr } from "date-fns/locale";
+
+interface Props {
+  country: string;
+  date: Date;
+}
+
+export const Heading: FunctionComponent<Props> = ({ country, date }) => {
+  return (
+    <div className="flex justify-between items-center">
+      <div className="p-5 py-8 border-2 rounded-1 rounded-full">
+        <strong className="block text-12">{country}</strong>
+      </div>
+      <span className={"block text-12 text-gray-70"}>
+        Mise à jour le{" "}
+        {format(new Date(date), "P", {
+          locale: fr,
+        })}
+      </span>
+    </div>
+  );
+};
--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,8 +1,7 @@
 import React, { FunctionComponent } from "react";
-import { format } from "date-fns";
 import { Included } from "../../models/restriction";
 import { useModal } from "../../hooks/use-modal";
-import { fr } from "date-fns/locale";
+import { Heading } from "./heading";
 
 interface Props {
   restriction: Included;
@@ -12,71 +11,49 @@ export const Card: FunctionComponent<Props> = ({ restriction }: Props) => {
   const { show, RenderModal } = useModal();
   return (
     <>
-      <div
-        onClick={show}
-        className="shadow-high bg-gray-light rounded-5 py-10 rounded-tl-none pb-30 pt-30 pl-20 pr-20 text-14 max-h-300 cursor-pointer flex-col justify-between"
-      >
-        <div className={"flex items-center mb-10"}>
-          <strong className="text-12">{restriction.attributes.country}</strong>
-          <h3 className="ml-16 font-semibold ml-5">
-            {restriction.attributes.title}
-          </h3>
-        </div>
-        <div className={"mb-10"}>
-          <span className={"block text-12 text-gray-70"}>
-            Mise en vigueur :{" "}
-            {format(new Date(restriction.attributes.createdAt), "P", {
-              locale: fr,
-            })}
-          </span>
-          <span className={"block text-12 text-gray-70"}>
-            Dernière mise à jour :{" "}
-            {format(new Date(restriction.attributes.lastUpdatedAt), "P", {
-              locale: fr,
-            })}
-          </span>
-        </div>
-        {restriction.attributes.description && (
-          <p className="text-14 line-clamp-4">
-            {restriction.attributes.description}
-          </p>
-        )}
-        <div className="flex justify-between self-end">
-          <a
-            href={restriction.attributes.source.url}
-            target="_blank"
-            rel="noreferrer"
-            className="text-12 text-red hover:text-dark"
-          >
-            {restriction.attributes.source.title}
-          </a>
-          <div className="w-20 h-20">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+      <div className="shadow-high bg-gray-light rounded-5 py-10 rounded-tl-none pb-30 pt-30 pl-20 pr-20 text-14 max-h-340 flex-col justify-between">
+        <div>
+          <div className="mb-20">
+            <Heading
+              country={restriction.attributes.country}
+              date={restriction.attributes.lastUpdatedAt}
+            />
+          </div>
+          <div className="">
+            <h3 className="font-semibold text-16">
+              {restriction.attributes.title}
+            </h3>
+          </div>
+          <div>
+            <p className="text-14 line-clamp-3">
+              {restriction.attributes.description}
+            </p>
+          </div>
+          <div className="self-end">
+            <a
+              href={restriction.attributes.source.url}
+              target="_blank"
+              rel="noreferrer"
+              className="text-12 text-red hover:text-dark"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M8 12h.01M12 12h.01M16 12h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
+              {restriction.attributes.source.title}
+            </a>
+            <button
+              className="block border-2 border-gray rounded-full py-6 px-10 uppercase font-semibold mt-20 hover:bg-gray hover:text-white transition ease-in ml-auto"
+              onClick={show}
+            >
+              En savoir plus
+            </button>
           </div>
         </div>
       </div>
       <RenderModal>
         <>
-          <div className={"flex items-center mb-10"}>
-            <strong className="text-12">
-              {restriction.attributes.country}
-            </strong>
-            <h3 className="ml-16 font-semibold ml-5">
-              {restriction.attributes.title}
-            </h3>
+          <div className="mb-20">
+            <Heading
+              country={restriction.attributes.country}
+              date={restriction.attributes.lastUpdatedAt}
+            />
           </div>
           {restriction.attributes.description && (
             <p className="text-14">{restriction.attributes.description}</p>
